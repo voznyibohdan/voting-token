f61d035ebc47a195d562dbfb9638c8fe2f62d036
voznyibohdan <bohdanvoznyi.em@gmail.com>
2024-01-10 09:10:25 +0200

    changed hardaht config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
+import * as dotenv from 'dotenv';
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-gas-reporter';
+
+dotenv.config();
+
+const REPORT_GAS = process.env.REPORT_GAS === 'true';
 
 const config: HardhatUserConfig = {
     solidity: '0.8.20',
+    networks: {
+        hardhat: {
+            blockGasLimit: 10000000,
+            allowUnlimitedContractSize: true,
+        },
+    },
+    gasReporter: {
+        enabled: REPORT_GAS,
+        currency: 'USD',
+        outputFile: 'gas-report.txt',
+    },
 };
 
 export default config;
