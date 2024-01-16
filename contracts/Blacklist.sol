55bc92df7ac259f72ef05b94a4d2202f3a0b2ac5
voznyibohdan <bohdanvoznyi.em@gmail.com>
2024-01-16 12:08:36 +0200

    blacklisted users dos attack

---
+++ contracts/Blacklist.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+contract Blacklist {
+    address[] public blacklist;
+    mapping(address => uint256) public lastUserTransferBlock;
+    mapping(address => bool) public blacklistedUsers;
+
+    function transfer() external {
+        lastUserTransferBlock[msg.sender] = block.number;
+        if (lastUserTransferBlock[msg.sender] >= 3) {
+            blacklist.push(msg.sender);
+        }
+    }
+
+    modifier notBlacklisted() {
+        bool userBlacklisted = false;
+        for (uint i = 0; i < blacklist.length; i++) {
+            if (blacklist[i] == msg.sender) {
+                userBlacklisted = true;
+                break;
+            }
+        }
+        require(!userBlacklisted, "user is in black list");
+    }
+
+//    modifier notBlacklisted() {
+//        require(!blacklistedUsers[msg.sender], "user is in black list");
+//    }
+
+    function someFunction() external {
+        if((block.number - lastUserTransferBlock[msg.sender]) >= 3) {
+            blacklist.push(msg.sender);
+//            blacklistedUsers[msg.sender] = true;
+        }
+    }
+
+    function buy() external notBlacklisted {
+        lastUserTransferBlock[msg.sender] = block.number;
+    }
+}
